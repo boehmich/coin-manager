857cf0962443d5cba9692b59946bc5eae647df83
Michael Böhm <michael_boehm@gmx.at>
2021-12-05 21:52:17 +0100

    Database and CoinDAO added

--- app/src/main/java/com/example/coinmanager/Coin.kt
+++ app/src/main/java/com/example/coinmanager/Coin.kt
@@ -1,11 +1,15 @@
 package com.example.coinmanager
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
 
+@Entity(tableName = "Coins")
 class Coin(
-    var id: Int = 0,
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     var apiId: Int,
     var name: String,
     var symbol: String,
     var slug: String,
     var pricePurchased: Double
-)
+): Serializable
